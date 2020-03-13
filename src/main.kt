55bcaeb78d6f5c58740cc2babfc0017b9a249857
cesarms89 <cesarms89@hotmail.com>
2020-03-13 09:41:41 -0600

    Add files via upload

---
+++ src/main.kt
@@ -0,0 +1,197 @@
+import java.io.File
+import kotlin.random.Random
+
+const val numEnt = 4
+const val numSal = 3
+
+fun main(){
+
+    val salidas: MutableList<DoubleArray> = mutableListOf()
+
+    val train: MutableList<DoubleArray> = mutableListOf()
+    File("C:\\Users\\Cesar\\Desktop\\training.data").forEachLine {
+        if (it.isNotEmpty()){
+            val tmp = it.split(",")
+            train.add(DoubleArray(tmp.size))
+            for (i in tmp.indices)
+                train.last()[i] = tmp[i].toDouble()
+            //--------------
+            if(tmp.last().toDouble() == 1.0)
+                salidas.add(doubleArrayOf(-1.0, -1.0, 1.0))
+            else if(tmp.last().toDouble() == 2.0)
+                salidas.add(doubleArrayOf(-1.0, 1.0, -1.0))
+            else if(tmp.last().toDouble() == 3.0)
+                salidas.add(doubleArrayOf(1.0, -1.0, -1.0))
+        }
+    }
+
+    //--------------
+    /*for (i in train.indices){
+        println("Clase: ${train[i].last()} \t Salida: ${salidas[i][0]}, ${salidas[i][1]}, ${salidas[i][2]}")
+    }*/
+    //--------------
+
+    //****************** ADALINA ******************
+    val umbral = 0.00001 //0.00001
+    var epoca = 0
+
+    val w = Array(numSal) { DoubleArray(numEnt) }
+    val rnd = Random
+    //Pesos aleatorios
+    for (i in 0 until numSal)
+        for (j in 0 until numEnt)
+            w[i][j] = rnd.nextDouble()
+
+    var EQMAnterior: DoubleArray
+    var EQMAtual: DoubleArray
+
+    //val limite = 10
+    var noAprende = 0
+    var errProm: Double
+    var errPromOld = 0.0
+
+    do {
+
+        EQMAnterior = LMS(train, salidas, w)
+        errProm = 0.0
+        for (i in 0 until numSal){
+            println("Error $i: ${EQMAnterior[i]}")
+            errProm += EQMAnterior[i]
+        }
+        errProm /= EQMAnterior.size
+        println("Error Promedio $errProm")
+        println("--------------------------------")
+
+        //Busqueda local
+        /*for (i in 0 until numSal){
+            for (j in 0 until numEnt){
+                val r = rnd.nextDouble()
+                val o = w[i][j]
+                w[i][j] = r
+                EQMAtual = LMS(train, salidas, w)
+
+                var nerrProm=0.0
+                for (k in 0 until numSal){
+                    nerrProm += EQMAtual[k]
+                }
+                nerrProm /= EQMAtual.size
+
+                if (nerrProm < errProm){
+                    w[i][j] = r
+                    errProm = nerrProm
+                }
+                else
+                    w[i][j] = o
+            }
+        }*/
+        //-----------------------------
+
+        for (i in train.indices){
+            val u = DoubleArray(numSal)
+            //val u = doubleArrayOf(-1.0, -1.0, -1.0)
+            for (j in 0 until numEnt){
+                for (k in 0 until numSal){
+                    u[k] += w[k][j] * train[i][j]
+                }
+            }
+
+            for (j in 0 until numEnt){
+                for (k in 0 until numSal){
+                    w[k][j] = w[k][j] + umbral * (salidas[i][k] - u[k]) * train[i][j]
+                }
+            }
+        }
+
+        epoca++
+        if (errPromOld != errProm){
+            errPromOld = errProm
+            noAprende = 0
+        }
+        else
+            noAprende++
+//
+    }while (epoca < 100000)
+    //}while (noAprende < 10)
+
+    println("Epocas: $epoca")
+    println("--------------------------------")
+
+    //****************** TEST ******************
+    val salidaTest: MutableList<DoubleArray> = mutableListOf()
+    val test: MutableList<DoubleArray> = mutableListOf()
+    File("C:\\Users\\Cesar\\Desktop\\test.data").forEachLine {
+        if (it.isNotEmpty()){
+            val tmp = it.split(",")
+            test.add(DoubleArray(tmp.size))
+            for (i in tmp.indices)
+                test.last()[i] = tmp[i].toDouble()
+            //-----
+            if(tmp.last().toDouble() == 1.0)
+                salidaTest.add(doubleArrayOf(-1.0, -1.0, 1.0))
+            else if(tmp.last().toDouble() == 2.0)
+                salidaTest.add(doubleArrayOf(-1.0, 1.0, -1.0))
+            else if(tmp.last().toDouble() == 3.0)
+                salidaTest.add(doubleArrayOf(1.0, -1.0, -1.0))
+        }
+    }
+
+    var sum = 0;
+    //var resp: Double
+    for (item in test.indices){
+        val u = DoubleArray(numSal)
+        for (k in 0 until numSal){
+            for (i in 0 until numEnt){
+                u[k] += test[item][i] * w[k][i]
+            }
+        }
+        //resp = if (u >= 0) 1 else 0
+        //resp = u
+        //println("Real: ${item.last()} \t Calculada: $resp")
+        print("Real (${test[item].last()}): ")
+        /*for (k in 0 until numSal){
+            print("${salidaTest[item][k]}  ")
+        }*/
+
+        //Asignar la clase al valor mayor en la neurona
+        var clase = 0.0
+        if(u[0] > u[1] && u[0] > u[2])
+            /*u = doubleArrayOf(1.0, -1.0, -1.0);*/ clase=3.0
+        else if(u[1] > u[0] && u[1] > u[2])
+            /*u = doubleArrayOf(-1.0, 1.0, -1.0);*/ clase=2.0
+        if(u[2] > u[0] && u[2] > u[1])
+            /*u = doubleArrayOf(-1.0, -1.0, 1.0);*/ clase=1.0
+
+        if (test[item].last() == clase) sum++
+
+        print("    Calculada: $clase     ${if(test[item].last() != clase) "x" else ""}")
+        /*for (k in 0 until numSal){
+            //Escalon
+            u[k] = if (u[k]>0) 1.0 else -1.0
+            print("${u[k]}  ")
+        }*/
+        println()
+    }
+
+    println("Acieros: $sum    Efectividad: ${100.0/test.size.toDouble()*sum.toDouble()}")
+
+}
+
+
+fun LMS(train: MutableList<DoubleArray>, salidas: MutableList<DoubleArray>, w: Array<DoubleArray>): DoubleArray {
+    val lms = DoubleArray(numSal)
+    for (i in train.indices){
+        val u = DoubleArray(numSal)
+        for (j in 0 until numEnt){
+            for (k in 0 until numSal){
+                u[k] += w[k][j] * train[i][j]
+            }
+        }
+        for (k in 0 until numSal){
+            lms[k] += Math.pow(salidas[i][k] - u[k], 2.0)
+        }
+    }
+    for (k in 0 until numSal){
+        lms[k] = lms[k] / train.size
+    }
+    return lms
+}
---
+++ src/main2.kt
@@ -0,0 +1,105 @@
+import java.io.File
+import kotlin.math.roundToInt
+import kotlin.random.Random
+
+fun main(){
+
+    val train: MutableList<DoubleArray> = mutableListOf();
+    File("C:\\Users\\Cesar\\Desktop\\training.data").forEachLine {
+        if (it.isNotEmpty()){
+            val tmp = it.split(",")
+            train.add(DoubleArray(tmp.size))
+            for (i in tmp.indices)
+                train.last()[i] = tmp[i].toDouble()
+        }
+    }
+
+    //****************** ADALINA ******************
+    val umbral = 0.0001
+    val precision = 0.0
+    var epoca = 0
+
+    val w = DoubleArray(train.first().size-1)
+    val rnd = Random(1)
+    //Pesos aleatorios
+    for (i in w.indices)
+        w[i] = rnd.nextDouble()
+
+    var EQMAnterior: Double
+    var EQMAtual: Double
+
+    do {
+
+        EQMAnterior = ErrorCuadratico(train, w)
+
+        for (i in train.indices){
+            var u = 0.0
+            for (j in w.indices){
+                u += train[i][j] *  w[j]
+            }
+
+            for (j in w.indices){
+                w[j] = w[j] + umbral * (train[i].last()-u) * train[i][j]
+            }
+        }
+
+        epoca++
+
+        EQMAtual = ErrorCuadratico(train, w)
+
+    }while (Math.abs(EQMAtual - EQMAnterior) > precision)
+
+    println("Epoca: $epoca")
+    for (i in w.indices)
+        println("w$i: ${w[i]}")
+    println("----------------------------")
+
+
+    //****************** TEST ******************
+    val test: MutableList<DoubleArray> = mutableListOf();
+    File("C:\\Users\\Cesar\\Desktop\\test.data").forEachLine {
+        if (it.isNotEmpty()){
+            val tmp = it.split(",")
+            test.add(DoubleArray(tmp.size))
+            for (i in tmp.indices)
+                test.last()[i] = tmp[i].toDouble()
+        }
+    }
+
+    var contador = 0
+    var resp: Double
+    for (item in test){
+        var u = 0.0
+        for (i in w.indices){
+            u += item[i] * w[i]
+        }
+        //resp = if (u >= 0) 1 else 0
+        resp = u
+        if (item.last().toInt() == resp.roundToInt()) contador++
+        println("Real: ${item.last()} \t Calculada: ${resp.roundToInt()}   ${if(item.last().toInt() != resp.roundToInt()) "x" else ""}")
+    }
+    println("Aciertos: $contador    Efectividad: ${100.0/test.size.toDouble()*contador.toDouble()}")
+
+    /*val d1 = doubleArrayOf(1.0, 0.0, 0.0)
+    //val resp: Int
+    var u1 = 0.0
+    for (i in d1.indices)
+        u1 += d1[i] * w[i]
+    //resp = if (u1 >= .5) 1 else -1
+    print("Respuesta: $u1")*/
+
+}
+
+
+fun ErrorCuadratico(train: MutableList<DoubleArray>, w: DoubleArray ): Double {
+    var lms = 0.0
+    for (i in train.indices){
+        var u = 0.0
+        for (j in w.indices){
+            u += w[j] * train[i][j]
+        }
+        lms += Math.pow(train[i].last() - u, 2.0)
+    }
+    lms /= train.size
+    return lms
+}
